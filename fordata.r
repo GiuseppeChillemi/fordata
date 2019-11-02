5fef50e6f7607c9d4f22db54fe73d68b679ad95d
GiuseppeChillemi <giuseppe@chillemi.eu>
2019-11-02 11:20:01 +0100

    Update fordata.r

--- fordata.r
+++ fordata.r
@@ -4,10 +4,24 @@ Rebol [
 	Purpose: {Run a for loop on data-table with headings
 	The loop is run having the fields for each line available
 	as object elements on the word you have provided
-	A /WHERE refinement is available and you can specify a condition for the loop to be exectude
+	A /WHERE refinement is available and you can specify a condition for the loop to be exectude (ALL BLOCK condition)
 	}
-	Version: 1.0
+	Version: 1.1
 	Author: "Giuseppe Chillemi"
+	TBD: {
+		Investigate on the advantages of not using an object but a simple block to store row values
+		whenever possible, it should avoid the overhead of context creation which should be meaningful
+		for very large data blocks
+		
+		Investigate on a way to not make accessible to passed code other function words other than word object
+	}
+	History: {
+		1.1: Headings block (first data block) no longer copied
+		1.1: Added comments
+		1.1: Remove the row word from global context
+		1.1: added TBD and HISTORY to the header
+	}
+	Note: {}
 ]
 
 
@@ -23,21 +37,40 @@ fordata: func [
 	/where
 	"Condition for data filtering"
 	condition [block!]
-	"Block where you can use WORD/COLUMN in a condition to express valid ROWS"
+	"Block where you can use WORD/COLUMN in a condition to express valid ROWS (It is an ALL BLOCK condition)"
 	/local
 	headings
 	row-obj-specs
 ] 
 [
-	headings: copy first data-block
-	data-block: next data-block
-	row-obj-specs: copy []
+	
+	;=== Init phase ===
+	headings: first data-block																										;Get the headings on the first line of data
+	data-block: next data-block																										;Data block starts from the second block on
+	row-obj-specs: copy []																												;Init the specs block fort the object which will be
+																																								;	named as DATA-NAME arg content
+
+	;=== Main loop ===
+	;Iterates for all data
 	forall data-block [
+		
+		;Create the specs block assignining column values to object words which will have the same nae of the columns
 		forall  headings [
-			append row-obj-specs to-set-word first headings 
+			
+			;Iterate throught the headings and append a SET-WORD with heading name to the object spec
+			;ie: [Customer:]
+			append row-obj-specs to-set-word first headings
+			
+			;Append the value of the corresponding column to the object specs
+			;ie: [Costomer-name: "Amazon"] 
 			append row-obj-specs data-block/1/(index? headings)
 		]
+
+		;The following code creates the object with the provided specs
+		;Example: do [myrow: make object! [customer: "amazon"]] ;(if row name provided is MYROW)
 		do reduce [to-set-word data-name make object! row-obj-specs]
+		
+		; Executes the block of code provided to the function
 		either where = true [
 			if all condition [
 				do code-block
@@ -46,7 +79,9 @@ fordata: func [
 		[
 			do code-block			
 		]
-	
 	]
 
+	;=== Cleaning ====
+	;Remove the row object word from global word list
+	unset data-name
 ]
